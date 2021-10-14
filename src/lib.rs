9852d9391d8c5e6c681a2d2b308e31a706f706b4
frukhkamaa <81838584+frukhkamaa@users.noreply.github.com>
2021-10-14 08:48:49 -0400

    Add files via upload

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,8 @@
-use solana_program::{
-    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
-};
+// inside lib.rs, only the following line should be in here
+pub mod error;
+pub mod instruction;
+pub mod processor;
+pub mod state;
 
-entrypoint!(process_instruction);
-fn process_instruction(
-    program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    instruction_data: &[u8],
-) -> ProgramResult {
-    msg!(
-        "process_instruction: {}: {} accounts, data={:?}",
-        program_id,
-        accounts.len(),
-        instruction_data
-    );
-    Ok(())
-}
+#[cfg(not(feature = "no-entrypoint"))]
+pub mod entrypoint;
